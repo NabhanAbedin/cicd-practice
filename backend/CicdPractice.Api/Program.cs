75b9d07825bee822a70ad7c7d659feec95442f53
Nabhan <nabedi2006@gmail.com>
2026-08-08 12:19:02 -0400

    allowed enums to be used from strings

--- backend/CicdPractice.Api/Program.cs
+++ backend/CicdPractice.Api/Program.cs
@@ -1,3 +1,4 @@
+using System.Text.Json.Serialization;
 using CicdPractice.Api.Data;
 using CicdPractice.Api.Managers;
 using CicdPractice.Api.Repositories;
@@ -6,7 +7,9 @@ using Microsoft.EntityFrameworkCore;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-builder.Services.AddControllers();
+builder.Services.AddControllers()
+    .AddJsonOptions(options =>
+        options.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter()));
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
